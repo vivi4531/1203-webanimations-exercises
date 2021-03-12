b6fda9c5fc5a58055d5da895f9c81a90002a45d0
vivimortensen <vivi4531@stud.kea.dk>
2021-03-12 11:00:34 +0100

    1203 exercise 1-2-3 done

--- 2_waapi_bouncing.js
+++ 2_waapi_bouncing.js
@@ -1,5 +1,25 @@
 "use strict";
 
+const properties = {
+  duration: 1000, 
+  iterations: Infinity, 
+  direction: "alternate", 
+  easing: "ease-in-out"
+}; 
+
+//Create an array of keyframes with objects inside
+//Each keyframe is an object with properties to animate and values to animate them to 
+const keyframes = [
+{transform: 'translate(0,41.25vw)'}, 
+{transform: 'translate(0,0)'}
+]; 
+
+//Select the #ball (you could also use the class .bounce)
+const ball = document.querySelector("#ball"); 
+
+//Animate the ball with the keyframes and properties you just defined
+const animation = ball.animate(keyframes, properties); 
+
 /* 
   properties from the CSS
 .bounce {
--- 3_fall_stop_zoom.js
+++ 3_fall_stop_zoom.js
@@ -2,16 +2,50 @@
 
 const ball = document.querySelector("#ball");
 
-// Create keyframes and properties for falling and zoom
+// Create keyframes and properties for falling
+const propertiesFalling = {
+  duration: 1000, 
+  iterations: Infinity, 
+  // direction: "alternate", 
+  // easing: "ease-in-out"
+}; 
+
+const keyframesFalling = [
+  //Translates Y -20vw to 65vw, infinately, taking 1 second 
+  //Can also be transform: 'translate(0,-20vw)'
+  {transform: 'translateY(-20vw)'},
+  {transform: 'translateY(65vw)'}
+  ]; 
+
+  // Create keyframes and properties zoom
+const propertiesZoom = {
+  duration: 500, 
+  iterations: 1, 
+  // direction: "alternate", 
+  // easing: "ease-in-out", 
+  fill: 'forwards', 
+  composite: 'add'
+}; 
+
+const keyframesZoom = [
+  //Scales from 1 to 0 in a half second, only once, using fill:forwards
+  {transform: 'scale(1)'},
+  {transform: 'scale(0)'}
+  ]; 
 
 // register click
+document.querySelector("#ball").addEventListener("mousedown", ballClicked); 
 
 // start falling animation
+const fallAnimation = ball.animate(keyframesFalling, propertiesFalling);
 
 function ballClicked() {
   console.log("Ball clicked!");
   // pause falling animation
+  fallAnimation.pause();
 
   // start zoom-animation
+  const zoomAnimation = ball.animate(keyframesZoom, propertiesZoom);
+  
 
 }
